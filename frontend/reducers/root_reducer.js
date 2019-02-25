0981dca4c23b8b0e0e483721293861ca02aaa2a7
Sam <sam.d.odonnell@gmail.com>
2019-02-25 10:02:44 -0800

    Add actions and reducers

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,9 @@
+import { entitiesReducer } from "./entities_reducer";
+import { errorsReducer } from "./errors_reducer";
+import { sessionReducer } from "./session_reducer";
+
+export const rootReducer = combineReducers({
+    entities: entitiesReducer,
+    session: sessionReducer,
+    errors: errorsReducer
+})
---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,15 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS } from "../actions/session_actions";
+import { merge } from 'lodash';
+
+export const sessionErrorsReducer = (oldState={}, action) => {
+    switch(action.type) {
+        case RECEIVE_SESSION_ERRORS:
+            return merge({}, oldState, {errors: action.errors})
+        case RECEIVE_CURRENT_USER:
+            let newState = merge({}, oldState);
+            delete newState[errors]
+            return newState
+        default:
+            return oldState;
+    }
+}
